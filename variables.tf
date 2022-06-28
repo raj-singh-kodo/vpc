63c6ad7a1f569c02c1560b3064f64dff109f122c
raj-singh-kodo <108337706+raj-singh-kodo@users.noreply.github.com>
2022-06-28 20:03:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,38 +1,38 @@
 variable "namespace" {
   description = "Namespace (e.g. `cp` or `cloudposse`)"
-  type        = "string"
+  type        = string
 }
 
 variable "stage" {
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
-  type        = "string"
+  type        = string
 }
 
 variable "name" {
   description = "Name  (e.g. `app` or `cluster`)"
-  type        = "string"
+  type        = string
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "cidr_block" {
-  type        = "string"
+  type        = string
   description = "CIDR for the VPC"
 }
 
